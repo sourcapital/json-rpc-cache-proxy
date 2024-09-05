0303eb0169d15ae0620b4a90c609c0e97065142d
rekt0x <mail@rekt0x.com>
2024-09-05 10:46:12 +0800

    Use a broader subnet range

--- generate-nginx-config.sh
+++ generate-nginx-config.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Get the Docker network subnet
-DOCKER_SUBNET=$(ip route | grep default | awk '{print $3}' | cut -d. -f1-3)".0/24"
+DOCKER_IP=$(ip route | grep default | awk '{print $3}')
+DOCKER_SUBNET=$(echo $DOCKER_IP | cut -d. -f1-2).0.0/16
 
 # Generate the Nginx configuration file
 sed "s|DOCKER_SUBNET|$DOCKER_SUBNET|g" /usr/local/openresty/nginx/conf/nginx.conf.template > /usr/local/openresty/nginx/conf/nginx.conf
